76b1ee6357ffa82b9a4aa89fca9b7d49e35d8632
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-06-24 14:48:14 +0900

    Initial Commit

---
+++ FirebaseCodableDemo/FirebaseCodableDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseCodableDemoApp.swift
+//  FirebaseCodableDemo
+//
+//  Created by Yusuke Hasegawa on 2021/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseCodableDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
