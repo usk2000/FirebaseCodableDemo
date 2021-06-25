7779bf3f81c3c9816f0273cf73745a7a5d1c214e
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-06-25 13:47:17 +0900

    feat: Firestoreを追加

---
+++ FirebaseCodableDemo/ViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ViewModel.swift
+//  FirebaseCodableDemo
+//
+//  Created by Yusuke Hasegawa on 2021/06/24.
+//
+
+import Foundation
+import FirebaseFirestore
+import FirebaseCodable
+
+class ViewModel: ObservableObject {
+    
+}
